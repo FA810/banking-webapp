e4a5f371297ecc62b05362300a29f5f810348132
Fabio R <fabio.rizzello@tiscali.it>
2025-06-11 00:58:25 +0200

    delete user

--- src/main/java/com/bankingwebapp/controller/UserController.java
+++ src/main/java/com/bankingwebapp/controller/UserController.java
@@ -3,10 +3,13 @@ package com.bankingwebapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bankingwebapp.entity.User;
 import com.bankingwebapp.service.UserService;
@@ -35,4 +38,11 @@ public class UserController {
 		userService.saveUser(user);
 		return "redirect:/users";
 	}
+	
+	@PostMapping("/delete/{id}")
+	public String deleteUser(@PathVariable Long id) {
+	    userService.deleteUserById(id);
+	    return "redirect:/users";
+	}
+
 }
